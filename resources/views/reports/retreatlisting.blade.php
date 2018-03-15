393015c30bf3465ec08a0095c2a4da9533bff352
Anthony Borrow <anthony@moodle.org>
2018-03-14 21:07:33 -0500

    20180314 - adding email address and tweaking css for retreat listing report

--- resources/views/reports/retreatlisting.blade.php
+++ resources/views/reports/retreatlisting.blade.php
@@ -5,29 +5,29 @@
 @if (!$registrations->isEmpty())
 <h2>Retreat Listing for Retreat #{{$registrations[0]->retreat->idnumber}} - {{$registrations[0]->retreat->title}}</h2> 
      
-<hr />
  <table width="100%">
         <th class="row-1 row-name">Full name</th>
         <th class="row-2 row-address">Address</th>
-        <th class="row-3 row-city">City, State, Zip</th>
-        <th class="row-4 row-phone">Cell phone</th>
-        <th class="row-5 row-phone">Home phone</th>
-        <th class="row-6 row-phone">Parish</th>
+        <th class="row-3 row-phone">Cell phone</th>
+        <th class="row-4 row-phone">Home phone</th>
+        <th class="row-5 row-email">Email</th>
+        <th class="row-6 row-parish">Parish</th>
         <th class="row-7 row-notes">Notes</th>
     
                 
     @foreach($registrations as $registration)
-    
+
     <tr>
         <td>{{$registration->retreatant->display_name}}</td>
-        <td>{{$registration->retreatant->address_primary_street}}</td>
-        <td>{{$registration->retreatant->address_primary_city}}, {{$registration->retreatant->address_primary_state}}  {{$registration->retreatant->address_primary_postal_code}}</td>
+        <td>{{$registration->retreatant->address_primary_street}} {{$registration->retreatant->address_primary_city}}, {{$registration->retreatant->address_primary_state}}  {{$registration->retreatant->address_primary_postal_code}}</td>
         <td>{{$registration->retreatant->phone_home_mobile_number}}</td>
         <td>{{$registration->retreatant->phone_home_phone_number}}</td>
-        <td>{{$registration->retreatant->parish_name}}</td> 
-        <td>{{$registration->retreatant->note_regsitration_text}}</td>
-        
-    </tr>    
+        <td>{{$registration->retreatant->email_primary_text}}</td>
+        <td>{{$registration->retreatant->parish_name}}</td>
+        <td> </td>
+
+    </tr>
+       
     @endforeach
    @endIf    
 </table>
