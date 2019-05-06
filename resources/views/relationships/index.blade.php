de9b91bb91323a86a8008228550c518206c1278c
Anthony Borrow <anthony@moodle.org>
2019-05-05 22:08:19 -0500

    initial redesign of relationship index page

--- resources/views/relationships/index.blade.php
+++ resources/views/relationships/index.blade.php
@@ -1,51 +1,53 @@
 @extends('template')
 @section('content')
 
-    <section class="section-padding">
-        <div class="jumbotron text-left">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h1>
-                    <span class="grey">Relationship Index</span> 
-                    <span class="create"><a href={{ action('RelationshipController@create') }}>{!! Html::image('img/create.png', 'Add Relationship',array('title'=>"Add Relationship",'class' => 'btn btn-primary')) !!}</a></span></h1>
-                
-                </div>
-                @if ($relationships->isEmpty())
-                    <p>It is a brand new world, there are no relationships!</p>
-                @else
-                <table class="table table-bordered table-striped table-hover"><caption><h2>Relationship Types</h2></caption>
-                    <thead>
-                        <tr>
-                            <th>Description</th>
-                            <th>Relationship Type</th>
-                            <th>Contact A</th>
-                            <th>Contact B</th>
-                            <th>Start Date</th>
-                            <th>End Date</th>
-                            <th>Is Active</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($relationships as $relationship)
-                        <tr>
-                            <td><a href="relationship/{{ $relationship->id }}">{{ $relationship->description }}</a></td>
-                            <td>{{ $relationship->relationship_type_id }}</td>
-                            <td>{{ $relationship->contact_id_a }}</td>
-                            <td>{{ $relationship->contact_id_b }}</td>
-                            <td>{{ $relationship->start_date }}</td>
-                            <td>{{ $relationship->end_date }}</td>
-                            <td>{{ $relationship->is_active }}</td>
-                            <td>{{ $relationship->label_b_a }}</td>
-                            <td>N/A</td>
-                            
-                        </tr>
-                        @endforeach
-                        
-                    </tbody>
-                </table>
-                @endif
-                {!! $relationships->render() !!}
+<div class="row bg-cover">
+    <div class="col-12">
+        <h2>
+            Relationship Index
+            @can('create-relatioship')
+                <span class="options">
+                    <a href={{ action('RelationshipController@create') }}>
+                        <img src="{{ URL::asset('images/create.png') }}" alt="Add" class="btn btn-light" title="Add">
+                    </a>
+                </span>
+            @endCan
+        </h2>
+    </div>
+    <div class="col-12 my-3 table-responsive-md">
+        @if ($relationships->isEmpty())
+            <div class="col-12 text-center py-5">
+                <p>It is a brand new world, there are no relatioships!</p>
             </div>
-        </div>
-    </section>
+        @else
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Contact A</th>
+                        <th scope="col">Contact B</th>
+                        <th scope="col">Start date</th>
+                        <th scope="col">End date</th>
+                        <th scope="col">Active</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($relationships as $relationship)
+                    <tr>
+                        <td><a href="relationship/{{ $relationship->id}}">{{ $relationship->id }}</a></td>
+                        <td>{{ $relationship->relationship_type->name_a_b }}</td>
+                        <td>{{ $relationship->contact_a->display_name }}</td>
+                        <td>{{ $relationship->contact_b->display_name }}</td>
+                        <td>{{ $relationship->start_date }}</td>
+                        <td>{{ $relationship->end_date }}</td>
+                        <td>{{ $relationship->is_active }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+        {!! $relationships->render() !!}
+    </div>
+</div>
 @stop
